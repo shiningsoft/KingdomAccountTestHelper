da34547310ddfb73a42566d150dc9b77ec50ef10
sailfish <nuoyan_cfan@163.com>
2017-07-04 08:18:32 +0800

    添加项目文件。

---
+++ 金证统一账户测试账户生成器/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 金证统一账户测试账户生成器
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
